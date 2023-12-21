7d29474061d51342d53dae7473e0c680fea2629a
varun <varunannepu123@gmail.com>
2023-12-21 21:07:50 +0530

    tags & load balancer

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_security_group" "main" {
 
   ingress {
     description = "APP"
-    from_port   = 443
-    to_port     = 443
+    from_port   = var.sg_port
+    to_port     = var.sg_port
     protocol    = "tcp"
     cidr_blocks = var.sg_ingress_cidr
   }
